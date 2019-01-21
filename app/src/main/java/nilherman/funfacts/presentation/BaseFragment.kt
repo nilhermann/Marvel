c1dc29e9c763768718a4fa0bae7bcfb57c7e2a90
Nil Herman <nil.herman@basetis.com>
2019-01-21 15:31:35 +0100

    Update adds transparent statusbar in details view

--- app/src/main/java/nilherman/funfacts/presentation/BaseFragment.kt
+++ app/src/main/java/nilherman/funfacts/presentation/BaseFragment.kt
@@ -1,10 +1,22 @@
 package nilherman.funfacts.presentation
 
+import android.os.Build
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
 import nilherman.funfacts.R
 import nilherman.funfacts.presentation.detail.DetailsFragment
 
 open class BaseFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity?.let { activity ->
+                activity.window.statusBarColor = ContextCompat.getColor(activity, R.color.colorPrimaryDark)
+            }
+        }
+    }
 
     fun navigateToDetails() {
         val fragment = DetailsFragment()
